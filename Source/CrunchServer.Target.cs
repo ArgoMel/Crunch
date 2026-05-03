9b6e91651df9d5bd51df45eca1acf8d3b4ed4be3
ArgoMel <qex1357@naver.com>
2026-05-03 12:24:52 +0900

    5.7변경

--- Source/CrunchServer.Target.cs
+++ Source/CrunchServer.Target.cs
@@ -8,8 +8,8 @@ public class CrunchServerTarget : TargetRules
 	public CrunchServerTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Crunch");
 	}
 }
